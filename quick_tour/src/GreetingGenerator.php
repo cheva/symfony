ce5f8c1b85c7c96310b50d245fce77cea07fff4d
Dee <dee.covan@gmail.com>
2024-10-05 17:37:17 +0300

    Continue with src/Twig/GreetExtension

--- quick_tour/src/GreetingGenerator.php
+++ quick_tour/src/GreetingGenerator.php
@@ -2,11 +2,21 @@
 // src/GreetingGenerator.php
 namespace App;
 
+use Psr\Log\LoggerInterface;
+
 class GreetingGenerator
 {
+    public function __construct(
+        private LoggerInterface $logger,
+    )
+    {
+    }
+
     public function getRandomGreeting(): string
     {
-        $greetings = ['Hey', 'Yo', 'Aloha'];
-        return $greetings[array_rand($greetings)];
+        $greetings = ['Hey', 'Yo', 'Aloha'];    
+        $greeting = $greetings[array_rand($greetings)]
+        $this->logger->info('Using the greeting: ' . $greeting);
+        return $greeting;
     }
 }
