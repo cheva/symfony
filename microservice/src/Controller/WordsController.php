c82b374f2bcdcd832fbe551dbfada3a1fac3546e
Dee <dee.covan@gmail.com>
2024-10-17 14:08:19 +0300

    Deploy Post /words/{word}/synonyms Response word:qwerty

--- microservice/src/Controller/WordsController.php
+++ microservice/src/Controller/WordsController.php
@@ -2,7 +2,15 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
 class WordsController
 {
-
+    #[Route('/words/{word}/synonyms', name: 'synonyms', methods: 'POST')]
+    public function synonyms(string $word): \Symfony\Component\HttpFoundation\Response {
+        return new JsonResponse([
+            'word' => $word,
+        ], 200);
+    }
 }
