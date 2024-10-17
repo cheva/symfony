11d9ef41ba1d9e5783e62448901bddd6a894e459
Dee <dee.covan@gmail.com>
2024-10-17 14:26:31 +0300

    Deploy Headers processing

--- microservice/src/Controller/WordsController.php
+++ microservice/src/Controller/WordsController.php
@@ -3,12 +3,21 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 class WordsController
 {
     #[Route('/words/{word}/synonyms', name: 'synonyms', methods: 'POST')]
-    public function synonyms(string $word): \Symfony\Component\HttpFoundation\Response {
+    public function synonyms(Request $request, string $word): \Symfony\Component\HttpFoundation\Response
+    {
+        if ($request->headers->has('force_fail')) {
+            return new JsonResponse([
+                'error' => 'Forced Fail',
+            ],
+                $request->headers->get('force_fail'));
+        }
+
         return new JsonResponse([
             'word' => $word,
         ], 200);
